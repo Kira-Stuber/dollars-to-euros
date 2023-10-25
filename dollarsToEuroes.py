0c1cc2d7a96bd576e758b61cea6636a8c112d899
Kira-Stuber <ks120508@wgcloud.org>
2023-10-25 11:24:39 -0500

    step 5

--- dollarsToEuroes.py
+++ dollarsToEuroes.py
@@ -1,2 +1,8 @@
-dollar = float(input("Enter dollar amount to be converted: "))
-euros = dollar * .94540
+convert = input("Would you like to convert US dollars to euros?: ")
+
+while convert == "yes":
+    dollar = float(input("Enter dollar amount to be converted: "))
+    euros = dollar * .94540
+    euros = str(euros)
+    print("€" + euros)
+    convert = input("Would you like to convert US dollars to euros?: ")
